9f4ae17d641fd6d4c1db96b483bab34e4111f199
franeum <elettrochuck@gmail.com>
2022-02-22 16:40:13 +0100

    fix

--- test.py
+++ test.py
@@ -7,18 +7,19 @@ import mraa
 PIN1 = 11
 PIN2 = 13
 
-#encoder = re.RotaryEncoder(PIN1, PIN2, re.LATCHMODE['TWO03'])
 
-enc = 0
+class Tastoma:
+    def __init__(self, p1, p2):
+        self._enc = renc.RotaryEncoder(p1, p2, renc.LATCHMODE['TWO03'])
 
 
 def isr_routine(gpio):
-    #enc.tick()
-    #print(gpio.read())
-    #print()
-    #print()
-    print(f"pin: {gpio.getPin(True)}\tvalue: {gpio.read()}")
-    print()
+    enc.tick()
+    # print(gpio.read())
+    # print()
+    # print()
+    # print(f"pin: {gpio.getPin(True)}\tvalue: {gpio.read()}")
+    # print()
 
 
 print("Test with rotary encoder")
@@ -26,7 +27,9 @@ x = mraa.Gpio(PIN1)
 y = mraa.Gpio(PIN2)
 x.dir(mraa.DIR_IN)
 y.dir(mraa.DIR_IN)
-enc = renc.RotaryEncoder(x, y, renc.LATCHMODE['TWO03'])
+
+enc = Tastoma(x, y)
+
 x.isr(mraa.EDGE_BOTH, isr_routine, x)
 y.isr(mraa.EDGE_BOTH, isr_routine, y)
 

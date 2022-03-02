df980194a0b8351f5a0fd0d3003ff46fc1488892
franeum <elettrochuck@gmail.com>
2022-03-03 00:22:23 +0100

    updated

--- _test.py
+++ _test.py
@@ -2,7 +2,13 @@
 
 import rotary_encoder as renc
 import mraa
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
 
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
 
 PIN1 = 11
 PIN2 = 13
@@ -30,12 +36,27 @@ x.isr(mraa.EDGE_BOTH, isr_routine, x)
 y.isr(mraa.EDGE_BOTH, isr_routine, y)
 
 pos = 0
-lastdir = None
+a_pos = 0
+
+names = ["A","B","C","D","E","F"]
+
+with canvas(device) as draw:
+    draw.text((30, a_pos), "REVERB", fill="white")
 
 while True:
     enc._enc.tick()
     new_pos = enc._enc.get_position()
+    direction = int(enc._enc.get_direction())
+
     if pos != new_pos:
         print(f"pos: {new_pos}\tdir: {int(enc._enc.get_direction())}")
         pos = new_pos
+        if direction == 1:
+            a_pos += 1
+        else:
+            a_pos -= 1
+        with canvas(device) as draw:
+            for i in range(6):
+                draw.text((30, i * 10), names[(a_pos+i)%6], fill="white")
+
 

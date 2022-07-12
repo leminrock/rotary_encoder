739247b39e0b893c6a008f89214933565402936c
franeum desk <elettrochuck@gmail.com>
2022-07-12 18:09:11 +0200

    update

--- udp_server.py
+++ udp_server.py
@@ -15,7 +15,9 @@ tm = _tm1637.TM1637(clk=CLK, dio=DIO)
 
 def filter_handler(address, *args):
     #print(f"{address}: {args}")
-    tm.show(str(args[0]).rjust(4))
+    value = int(args[0])
+    value = str(value)
+    tm.show(value.rjust(4))
 
 
 dispatcher = Dispatcher()

d5831a8b581fc33620bf1159dd85c008e3029d16
neum <elettrochuck@gmail.com>
2022-07-16 01:06:05 +0200

    set proto

--- udp_rotary/py_server.py
+++ udp_rotary/py_server.py
@@ -13,6 +13,7 @@ tm = _tm1637.TM1637(clk=CLK, dio=DIO)
 
 
 def value_handler(unused_addr, *args):
+    """callback function"""
     value = args[0]
     value = str(int(value))
     tm.show(value.rjust(4))
@@ -26,10 +27,3 @@ if __name__ == '__main__':
     server = osc_server.ThreadingOSCUDPServer((IP, IN_PORT), dispatcher)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
-
-    """
-    while True:
-        client.send_message("/filter", random.random())
-        print("send")
-        time.sleep(1)
-    """

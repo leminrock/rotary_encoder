858809a99eef7c6aab5ce5c64600f2006d31e60d
franeum <elettrochuck@gmail.com>
2022-07-17 14:15:58 +0200

    update i2c device

--- udp_rotary/py_server.py
+++ udp_rotary/py_server.py
@@ -6,8 +6,8 @@ from pythonosc import osc_server
 
 IP = '127.0.0.1'
 IN_PORT = 8000
-CLK = 24
-DIO = 23
+CLK = 13
+DIO = 11 
 ADDRESS = ['/rotary_0', '/rotary_1']
 
 tm = _tm1637.TM1637(clk=CLK, dio=DIO)
@@ -16,9 +16,10 @@ tm = _tm1637.TM1637(clk=CLK, dio=DIO)
 def value_handler(unused_addr, *args):
     """callback function"""
     value = args[0]
-    value = str(int(value))
-    tm.show(value.rjust(4))
-
+    #value = str(int(value))
+    #tm.show(value.rjust(4))
+    value = int(value)
+    tm.number(value)
 
 if __name__ == '__main__':
 
